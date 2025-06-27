c13d29ad4664bca65cddc6c42d47e0c2a6df2240
lmohade3l <mohade3.2710@gmail.com>
2025-06-27 14:20:39 +0330

    feat: add postNote fn to write to the database

--- src/app/write/page.js
+++ src/app/write/page.js
@@ -4,7 +4,7 @@ import postNote from "./postNote";
 export default async function WritePage() {
   async function getUsers() {
     const db = await AsyncDatabase.open("./notes.db");
-    db.all("SELECT * FROM users");
+    return db.all("SELECT * FROM users");
   }
 
   const users = await getUsers();
